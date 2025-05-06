9c2fae9acf5a084988b050cedb93dfa7691f6ad5
Finbar Dennehy <finny.d@gmail.com>
2025-05-06 01:14:19 +0100

    complete assignment 4

--- assignment04-github.py
+++ assignment04-github.py
@@ -1,40 +1,37 @@
-# to use this install package
-# pip install PyGithub
+# Program that takes a file from a repo, replaces strings in that file, and then commits the updated file back to the repo
+# Author: Finbar Dennehy
+
+# To use this install package, pip install PyGithub
 
 from github import Github
-from config import config as cfg
+from config import config as cfg # config file containing API key, not pushed to git.
 import requests
 
+# Github API key
 apikey = cfg["githubkey"]
-# use your own key
 g = Github(apikey)
 
-# Get my own repos
-#for repo in g.get_user().get_repos():
-    #print(repo.name)
-
-# Get some else's (public) repos
-#for repo in g.get_user("fdennehy")
-    #print(repo.name)
-
-repo = g.get_repo("fdennehy/aprivateone")
-#print(repo.clone_url)
-
-fileInfo = repo.get_contents("test.txt")
+# set variables for repository and file.
+repo = g.get_repo("fdennehy/wsaa-assignments")
+filename = "Andrew.txt"
+fileInfo = repo.get_contents(filename)
 urlOfFIle = fileInfo.download_url
-#print(fileInfo)
-#print(urlOfFIle)
 
+# use requests module to store contents of file.
 response = requests.get(urlOfFIle)
 contentOfFile = response.text
-print(contentOfFile)
+# print(contentOfFile)
 
-# Append the text more stuff (with a newline character) to the contents of the file
-newContents = contentOfFile + " more stuff \n"
-print(newContents)
+# Set string to be replaced (string1) and it's replacement (string2)
+string1 = 'Andrew'
+string2 = 'Finbar'
 
-# Update the contents of the file on git up by using the function 
-# update_file(path, message, content, sha, branch=NotSet, committer=NotSet, author=NotSet)
+# Update the content of the file using string replace
+# https://docs.python.org/3/library/stdtypes.html#str.replace
+newContents = contentOfFile.replace(string1,string2)
+# print(newContents)
 
+# Update the contents of the file on git up by using the function: update_file(path, message, content, sha, branch=NotSet, committer=NotSet, author=NotSet)
 gitHubResponse=repo.update_file(fileInfo.path,"updated by prog", newContents,fileInfo.sha)
-print (gitHubResponse)
+print(f"'{string1}' has been replaced by '{string2}' in github.com/{repo.full_name}/{filename}. Github response:") 
+print(gitHubResponse)
